c70e6f6585b48c9ec5f02e64ed83fc545ba34b41
realsnoopso <real@snoop.so>
2024-10-14 18:08:11 +0900

    Fix select styles

--- src/components/chat-view/chat-input/ModelSelect.tsx
+++ src/components/chat-view/chat-input/ModelSelect.tsx
@@ -1,14 +1,17 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import { useState } from 'react'
 import { CHAT_MODEL_OPTIONS } from 'src/constants'
 import { useSettings } from 'src/contexts/settings-context'
 
 export function ModelSelect() {
   const { settings, setSettings } = useSettings()
-
+  const [isOpen, setIsOpen] = useState(false)
   return (
-    <DropdownMenu.Root>
-      <DropdownMenu.Trigger asChild>
-        <button>{settings.chatModel}</button>
+    <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenu.Trigger className="smtcmp-chat-input-model-select">
+        {settings.chatModel}
+        {isOpen ? <ChevronUp size={10} /> : <ChevronDown size={10} />}
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
