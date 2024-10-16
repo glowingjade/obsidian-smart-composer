d930d3c522d793f63b595db3e0000f192377dabb
Heesu Suh <hssuh96@gmail.com>
2024-10-16 17:56:50 +0900

    version up to 0.1.1

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,10 @@
 import { readFileSync, writeFileSync } from 'fs'
 
-const targetVersion = process.env.npm_package_version
+const targetVersion = process.argv[2]
+if (!targetVersion) {
+    console.error('Please provide a target version as a command line argument.')
+    process.exit(1)
+}
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
@@ -12,3 +16,8 @@ writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
 writeFileSync('versions.json', JSON.stringify(versions, null, '\t'))
+
+// update package.json with target version
+let packageJson = JSON.parse(readFileSync('package.json', 'utf8'))
+packageJson.version = targetVersion
+writeFileSync('package.json', JSON.stringify(packageJson, null, '\t'))
