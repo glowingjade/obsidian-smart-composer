2f272ff17d39e6e499e300acd69bf508fbb55378
Heesu Suh <hssuh96@gmail.com>
2024-10-16 17:59:22 +0900

    fix indent

--- version-bump.mjs
+++ version-bump.mjs
@@ -10,14 +10,14 @@ if (!targetVersion) {
 let manifest = JSON.parse(readFileSync('manifest.json', 'utf8'))
 const { minAppVersion } = manifest
 manifest.version = targetVersion
-writeFileSync('manifest.json', JSON.stringify(manifest, null, '\t'))
+writeFileSync('manifest.json', JSON.stringify(manifest, null, 2))
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync('versions.json', 'utf8'))
 versions[targetVersion] = minAppVersion
-writeFileSync('versions.json', JSON.stringify(versions, null, '\t'))
+writeFileSync('versions.json', JSON.stringify(versions, null, 2))
 
 // update package.json with target version
 let packageJson = JSON.parse(readFileSync('package.json', 'utf8'))
 packageJson.version = targetVersion
-writeFileSync('package.json', JSON.stringify(packageJson, null, '\t'))
+writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
