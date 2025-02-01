625df197eb3d012859cbed4d9da4a59405a22c25
Heesu Suh <hssuh96@gmail.com>
2025-02-01 14:27:32 +0700

    add migration to list

--- src/settings/schema/migrations/index.ts
+++ src/settings/schema/migrations/index.ts
@@ -1,6 +1,7 @@
 import { SettingMigration } from '../setting.types'
 
 import { migrateFrom0To1 } from './0_to_1'
+import { migrateFrom1To2 } from './1_to_2'
 
 export const SETTING_MIGRATIONS: SettingMigration[] = [
   {
@@ -8,5 +9,9 @@ export const SETTING_MIGRATIONS: SettingMigration[] = [
     toVersion: 1,
     migrate: migrateFrom0To1,
   },
-  // TODO: implement 1 to 2 migration
+  {
+    fromVersion: 1,
+    toVersion: 2,
+    migrate: migrateFrom1To2,
+  },
 ]
